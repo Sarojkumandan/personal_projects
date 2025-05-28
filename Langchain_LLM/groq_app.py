6a46fa8ea505a36247a49689dc3d6ea4487e66d0
Ashok Sai Doredla <ashoksaidoredla@Ashoks-MacBook-Air.local>
2025-05-28 10:43:29 -0400

    Langchain

---
+++ Langchain_LLM/groq_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import os
+from langchain_groq import chatgroq
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chains
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
